51a4f7fcf8c4d4fcf42d9549071dee37d53a9e37
Vane <avbica.1986@gmail.com>
2023-02-25 13:41:16 -0300

    Se creo un formulario nuevo

--- js/script.js
+++ js/script.js
@@ -1,48 +1,21 @@
-function GeneraForm(){
-    let formulario=document.createElement("form");
-
-    let titulo=document.createElement("label");
-
-    let cajaTextNombre=document.createElement("input");
-
-    let cajaTextApellido=document.createElement("input");
-
-    let cajaTextEmail=document.createElement("input");
-
-    let cajaTextAsunto=document.createElement("input");
-
-    let cajaTextMensaje=document.createElement("textarea");
-
-    let boton=document.createElement("input"); 
-
-        formulario.setAttribute('method', "post");
-        formulario.setAttribute('action', "");
-
-        cajaTextNombre.setAttribute('type', "text");
-        cajaTextNombre.setAttribute('placeholder', "Nombre");
-
-        cajaTextApellido.setAttribute('type', "text");
-        cajaTextApellido.setAttribute('placeholder', "Apellido");
-
-        cajaTextEmail.setAttribute('type', "text");
-        cajaTextEmail.setAttribute('placeholder', "Email");
-
-        cajaTextAsunto.setAttribute('type', "text");
-        cajaTextAsunto.setAttribute('placeholder', "Asunto");
-
-        cajaTextMensaje.setAttribute('placeholder', "Mensaje");
-
-    boton.setAttribute('type', "button");	
-        boton.setAttribute('value', "Enviar");
-        boton.setAttribute('onclick', "alert('Se envio el mensaje')");	
-
-        titulo.innerHTML='<h1>CONTACTAME</h1>';
-        formulario.appendChild(titulo);
-        formulario.appendChild(cajaTextNombre);
-        formulario.appendChild(cajaTextApellido);
-        formulario.appendChild(cajaTextEmail);
-        formulario.appendChild(cajaTextAsunto);
-        formulario.appendChild(cajaTextMensaje);
-        formulario.appendChild(boton);
-        document.getElementById("formulario").appendChild(formulario);
+var inputs= document.getElementsByClassName ("formulario_input");
+for (var i=0; i<inputs.length; i++){
+    inputs [i].addEventListener ("keyup", function() {
+        if (this.value.length>=1){ 
+            this.nextElementSibling.classList.add("fijar");
+        } else {
+            this.nextElementSibling.classList.remove("fijar");
+        }
+    });
 }
+
+const form= document.querySelector("form")
+document.querySelector(".show").addEventListener("click", () => {
+    form.classList.add("form_mostrar")
+})
+
+document.querySelector(".formulario_submit").addEventListener("click", () => {
+    form.classList.remove("form_mostrar")
+})
+
+
